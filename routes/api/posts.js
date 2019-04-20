c998ae3a6801b423939358f502d68a5acfc4eb34
Madeleine Woodbury <randine25@gmail.com>
2019-04-20 11:07:33 -0400

    Get all posts route and get single post by id route implemented

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,6 +15,25 @@ const User = require("../../models/User");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get single posy by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404));
+});
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
